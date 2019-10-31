3233c048da725f5a7d0485393ab1e8670fef56d0
book <bookgvi@gmail.com>
2019-11-01 00:13:20 +0300

    UGOLOC: оптимизация HTTP методов

--- src/components/DataTable/TableRow.vue
+++ src/components/DataTable/TableRow.vue
@@ -92,7 +92,6 @@ export default {
       if (active) classNames.push('active')
       if (classes) classNames.push(classes)
       if (__tdClass) classNames.push(__tdClass)
-      tableHeaderClass = ''
       return {
         class: classNames,
         style
